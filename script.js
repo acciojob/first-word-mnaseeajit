5121f7d9fe65a353bc23594cec7b81102b1ba2d2
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-13 13:09:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function firstWord(s) {
   // your code here
+	s = s.trimStart();
+	let index = 0;
+	for(let i = 0;i<s.length;i++)
+		{
+			if(s[i] == " "){
+				index = i;
+				return s.slice(0,index+1);
+			}
+		}   
+	for(let i = 0;i<s.length;i++)
+		{
+			if(s[i] != " "){
+				return s;
+			}
+		}
+	return "";
+	
 }
 
-// Do not change the code below
+// Do not change the code below 
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
